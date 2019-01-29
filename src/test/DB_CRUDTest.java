6186a17d162e5776166e3e59cda7444500a4ef83
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-01-29 13:03:53 +0100

    Added multiple test case todos

---
+++ src/test/DB_CRUDTest.java
@@ -0,0 +1,32 @@
+
+/**
+ * @author Marc Rohwedder Kær
+ * @date 29-01-2019
+ * Testing multiple CRUD operations
+ */
+public class DB_CRUDTest {
+    // TODO: UserCreate
+    // TODO: UserUpdate
+    // TODO: UserGet
+    // TODO: MultipleUserCreate
+    // TODO: MultipleUserGet
+    // TODO: UserDelete
+
+    // TODO: GameCreate
+    // TODO: GameGet
+    // TODO: GameUpdate
+    // TODO: GameDelete
+
+    // TODO: PlayerCreate
+    // TODO: PlayerUpdate
+    // TODO: PlayerGet
+    // TODO: PlayerDelete
+
+    // TODO: PlayerHistoryCreate
+    // TODO: PlayerHistoryUpdate
+    // TODO: PlayerHistoryGet
+    // TODO: PlayerHistoryMultipleCreate
+    // TODO: PlayerHistoryMultipleGet
+    // TODO: PlayerHistoryDelete
+}
+
