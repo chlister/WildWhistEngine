4952b53c0e824d648617a162f3266b28b7f92d72
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-01-29 13:03:25 +0100

    Added player class plus repository

---
+++ src/com/wildgroup/user_package/models/Player.java
@@ -0,0 +1,29 @@
+package com.wildgroup.user_package.models;
+
+import com.wildgroup.db_package.EntityObject;
+import java.sql.Blob;
+
+/**
+ * @author Marc Rohwedder Kær
+ * @date 29-01-2019
+ */
+public class Player extends EntityObject {
+    private String name;
+    private Blob avatar;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Blob getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Blob avatar) {
+        this.avatar = avatar;
+    }
+}
