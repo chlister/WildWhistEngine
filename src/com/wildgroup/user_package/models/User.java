513d25198e4425c58fa10ccbb67595963ffec013
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-01-29 12:44:37 +0100

    Game and User classes added. Both extends entityobject

--- src/com/wildgroup/user_package/models/User.java
+++ src/com/wildgroup/user_package/models/User.java
@@ -1,5 +1,7 @@
 package com.wildgroup.user_package.models;
 
+import com.wildgroup.db_package.EntityObject;
+
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -9,19 +11,32 @@ import java.util.Date;
  * @author Marc Rohwedder Kær
  * @date 29-01-2019
  */
-public class User {
-    private String name, email, password;
+public class User extends EntityObject {
+    private String firstName, middleName, lastName, email, password;
     private Date birthday;
-    int id;
 
-    //region Getters & setters
-
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -37,7 +52,7 @@ public class User {
     }
 
     public void setPassword(String password) {
-        MessageDigest md = null;
+        MessageDigest md;
         byte[] digest = null;
         try {
             // Defines the form of hashing used
