9cabbf703d33a9cccbf73d3f278860ab51915017
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-02-01 07:15:16 +0100

    Added DbModels for eliminating "magic strings"

---
+++ src/com/wildgroup/db_package/dbModels/UserDb.java
@@ -0,0 +1,15 @@
+package com.wildgroup.db_package.dbModels;
+
+/**
+ * @author Marc Rohwedder Kær
+ * @date 30-01-2019
+ */
+public class UserDb {
+    static public final String firstName = "first_name";
+    static public final String middleName = "middle_name";
+    static public final String lastName = "last_name";
+    static public final String email = "mail";
+    static public final String password= "password";
+    static public final String birthday = "birthday";
+    static public final String id = "user_id";
+}
