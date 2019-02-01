893b233e63538cdcf8dbb4a306fe96b1d44e077b
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-02-01 07:16:55 +0100

    Class is for eliminating "magic string" when typing table names

---
+++ src/com/wildgroup/db_package/TableNames.java
@@ -0,0 +1,12 @@
+package com.wildgroup.db_package;
+
+/**
+ * @author Marc Rohwedder Kær
+ * @date 29-01-2019
+ */
+public enum TableNames {
+    users,
+    game,
+    player,
+    playerhistory
+}
