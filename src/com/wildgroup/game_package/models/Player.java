0368416872fbdbcd25066b8804a5e314fa2ad04d
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-02-05 08:34:54 +0100

    Updated player model with Id

--- src/com/wildgroup/game_package/models/Player.java
+++ src/com/wildgroup/game_package/models/Player.java
@@ -6,9 +6,15 @@ package com.wildgroup.game_package.models;
  */
 public class Player {
     String name;
+    int id;
 
-    public Player(String name) {
+    public Player(String name, int id) {
         this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
